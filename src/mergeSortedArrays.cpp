5de5ff5aeea1e257f2dfac3422185b37af7f2334
srujanbelde <srujanbelde@missionrnd.com>
2016-01-13 14:15:25 +0530

    Submit Commit

--- src/mergeSortedArrays.cpp
+++ src/mergeSortedArrays.cpp
@@ -22,5 +22,60 @@ struct transaction {
 };
 
 struct transaction * mergeSortedArrays(struct transaction *A, int ALen, struct transaction *B, int BLen) {
-	return NULL;
+	if (A == NULL || B == NULL)
+		return NULL;
+	int i=0, j=0,c=0;
+	struct transaction C[100];
+	while(i < ALen&&j < BLen)
+	{
+		int day1 = ((A[i].date[0] - '0') * 10) + (A[i].date[1] - '0');
+		int month1 = ((A[i].date[3] - '0') * 10) + (A[i].date[4] - '0');
+		int year1 = ((A[i].date[6] - '0') * 1000) + ((A[i].date[7] - '0') * 100) + ((A[i].date[8] - '0') * 10) + (A[i].date[9] - '0');
+		int day2 = ((B[j].date[0] - '0') * 10) + (B[j].date[1] - '0');
+		int month2 = ((B[j].date[3] - '0') * 10) + (B[j].date[4] - '0');
+		int year2 = ((B[j].date[6] - '0') * 1000) + ((B[j].date[7] - '0') * 100) + ((B[j].date[8] - '0') * 10) + (B[j].date[9] - '0');
+		if (year1 <= year2)
+		{
+			if (year1 < year2)
+				C[c++] = A[i++];
+			else
+			{
+				if (month1 <= month2)
+				{
+					if (month1<month2)
+						C[c++] = A[i++];
+					else
+					{
+						if (day1 <= day2)
+						{
+							if (day1 < day2)
+								C[c++] = A[i++];
+							else
+							{
+								C[c++] = A[i++];
+								C[c++] = B[j++];
+							}
+						}
+						else
+							C[c++] = B[j++];
+					}
+				}
+				else
+					C[c++] = B[j++];
+			}
+		}
+		else
+			C[c++] = B[j++];
+	}
+	if (i < ALen)
+	{
+		for (int k = i; k < ALen; k++)
+			C[c++] = A[k];
+	}
+	if (j < BLen)
+	{
+		for (int k = j; k < BLen; k++)
+			C[c++] = B[k];
+	}
+	return C;
 }
